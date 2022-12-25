ca891facaf8589aa1acdfdbfef12f232bfdcdd17
bivek1 <bivekdhakal18@gmail.com>
2022-12-25 16:07:25 +0545

    again

--- news/homepage/urls.py
+++ news/homepage/urls.py
@@ -6,5 +6,7 @@ app_name = "homepage"
 urlpatterns= [
     path('', views.Homepage, name="homepage"),
     path('login', views.Login, name ="login"),
-    path('news-details/<int:id>', views.newsDetails, name = "newsDetails")
+    path('news-details/<int:id>', views.newsDetails, name = "newsDetails"),
+    path('news/<str:str>', views.selectNews, name = "selectNews"),
+    path('category/<int:id>/<slug:slug>', views.categoryNews, name = "categoryNews")
 ]
--- news/homepage/views.py
+++ news/homepage/views.py
@@ -9,37 +9,45 @@ from django.http import HttpResponseRedirect
 def Homepage(request):
     category = Category.objects.all()
     template = 'news/homepage.html'
-    news = News.objects.all().order_by('-id')[:6]
-    hot = News.objects.all().order_by('-id')[:4]
+    news = News.objects.all().order_by('-id')[:13]
+    normal = News.objects.filter(status = 'Normal').order_by('-id')[:12]
+    hot = News.objects.filter(status = 'Hot').order_by('-id')[:12]
     print(hot)
-    latest = News.objects.all().order_by('-id')[:5]
+    latest = News.objects.all().order_by('-id')[:20]
+    watched = News.objects.all().order_by('-total_visit')[:15]
     dist ={
         'category':category,
         'news_all':news,
         'latest':latest,
-        'hot':hot
+        'hot':hot,
+        'normal':normal,
+        'watched':watched
     }
 
     return render(request, template, dist)
 
 def newsDetails(request, id):
     news = News.objects.get(id = id)
-    latest = News.objects.all().order_by('-id')[:5]
-    category = Category.objects.all().order_by('-id')[:8]
+    
+    latest = News.objects.all().order_by('-id')[:11]
+    category = Category.objects.all().order_by('-id')[:10]
     other = News.objects.all().order_by('?')[:10]
+   
     dist = {
         'category':category,
         'news_all':news,
         'latest':latest,
         'other':other
     }
+    news.total_visit += 1
+    news.save()
 
     return render(request, "news/news_details.html", dist)
 
 def Login(request):
     template_name = "manager/login.html"
-    latest = News.objects.all().order_by('-id')[:5]
-    category = Category.objects.all().order_by('-id')[:8]
+    latest = News.objects.all().order_by('-id')[:11]
+    category = Category.objects.all().order_by('-id')[:10]
     dist = {
         'category':category,
         'latest':latest,
@@ -57,3 +65,41 @@ def Login(request):
     
     return render(request, template_name,dist)
    
+def selectNews(request, str):
+    category = Category.objects.all()[:10]
+    template = 'news/select_news.html'
+    news = News.objects.filter(status = str).order_by('-id')
+    hot = News.objects.filter(status = 'Hot').order_by('-id')[:10]
+   
+    normal = News.objects.filter(status = 'Normal').order_by('-id')[:10]
+    latest = News.objects.all().order_by('-id')[:15]
+    dist ={
+        'category':category,
+        'news_all':news,
+        'latest':latest,
+        'hot':hot,
+        'selected':str,
+        'normal':normal
+    }
+
+    return render(request, template, dist)
+
+
+def categoryNews(request, id, slug):
+    category = Category.objects.all()[:10]
+    cat = Category.objects.get(id = id)
+    template = 'news/category_news.html'
+    news = News.objects.filter(category = cat).order_by('-id')
+ 
+    normal = News.objects.filter(status = 'Normal').order_by('-id')[:10]
+    latest = News.objects.all().order_by('-id')[:15]
+    dist ={
+        'category':category,
+        'news_all':news,
+        'latest':latest,
+        'cat':cat,
+       
+        'normal':normal
+    }
+
+    return render(request, template, dist)
